61bbd59712c9658e97a98a16d5ec645d50f07404
Iago Augusto <iagoalvesaugusto@gmail.com>
2022-10-03 20:05:43 -0300

    Layout do do template do paciente principal e estilos

---
+++ view/paciente.js
@@ -0,0 +1,9 @@
+// object.addEventListener("click", myScript);
+
+function mudarSelecionar() {
+  document.getElementById('enfermagem').classList.add('icone-selecionado')
+}
+  
+let icone = document.getElementsByClassName('icone');
+
+
