6a8d59f4bbcb4c046152549b01fd6daf04fbc1b2
Iago Augusto <iagoalvesaugusto@gmail.com>
2022-10-14 05:31:54 -0300

    Adicionando informações do usuário

--- view/paciente.js
+++ view/paciente.js
@@ -1,9 +1,29 @@
-// object.addEventListener("click", myScript);
+object.addEventListener('click', myScript)
 
-function mudarSelecionar() {
-  document.getElementById('enfermagem').classList.add('icone-selecionado')
+function mudarItemSelecionadoPrincipal() {
+  document.getElementById('principal').classList.add('selecionado')
+  document.getElementById('anamnese').classList.remove('selecionado')
+  document.getElementById('evolucao').classList.remove('selecionado')
+  document.getElementById('consulta').classList.remove('selecionado')
 }
-  
-let icone = document.getElementsByClassName('icone');
 
+function mudarItemSelecionadoAnamnese() {
+  document.getElementById('anamnese').classList.add('selecionado')
+  document.getElementById('principal').classList.remove('selecionado')
+  document.getElementById('evolucao').classList.remove('selecionado')
+  document.getElementById('consulta').classList.remove('selecionado')
+}
 
+function mudarItemSelecionadoEvolucao() {
+  document.getElementById('evolucao').classList.add('selecionado')
+  document.getElementById('anamnese').classList.remove('selecionado')
+  document.getElementById('principal').classList.remove('selecionado')
+  document.getElementById('consulta').classList.remove('selecionado')
+}
+
+function mudarItemSelecionadoConsulta() {
+  document.getElementById('consulta').classList.add('selecionado')
+  document.getElementById('anamnese').classList.remove('selecionado')
+  document.getElementById('principal').classList.remove('selecionado')
+  document.getElementById('evolucao').classList.remove('selecionado')
+}
