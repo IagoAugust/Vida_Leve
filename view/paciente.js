87306ab687525bbb33967a64566114981b16320d
Iago Augusto <iagoalvesaugusto@gmail.com>
2022-10-20 00:42:26 -0300

    padronizando layout e criando padrão de navegações

--- view/paciente.js
+++ view/paciente.js
@@ -1,36 +1,38 @@
-const informacao = (document.querySelectorAll('.informacao'));
-informacao.forEach( aba => {
+const informacao = document.querySelectorAll('.informacao')
+informacao.forEach(aba => {
   aba.addEventListener('click', () => {
-
-    if(aba.classList.contains('selecionado')){
-      return;
+    if (aba.classList.contains('selecionado')) {
+      return
     }
-    
-    const informacaoSelecionado = document.querySelector('.informacao.selecionado');
-    informacaoSelecionado.classList.remove('selecionado');
-    aba.classList.add('selecionado');
 
+    const informacaoSelecionado = document.querySelector(
+      '.informacao.selecionado'
+    )
+    informacaoSelecionado.classList.remove('selecionado')
+    aba.classList.add('selecionado')
 
-    const sessaoSelecionado = document.querySelector('.abaConteudo.abaSelecionado');
-    sessaoSelecionado.classList.remove('abaSelecionado');
-    
+    const sessaoSelecionado = document.querySelector(
+      '.abaConteudo.abaSelecionado'
+    )
+    sessaoSelecionado.classList.remove('abaSelecionado')
 
-    const idContainerSessaoaba = `container-${aba.id}`;
-    const conteudoASerMostrado = document.getElementById(idContainerSessaoaba);
-    conteudoASerMostrado.classList.add('abaSelecionado');
-
-  });
-});
-
-
-const botaoAdicionar = document.querySelector('.adicionar');
-botaoAdicionar.addEventListener('click', () => {
-
-  const sessaoSelecionado = document.querySelector('.abaConteudo.abaSelecionado');
-  sessaoSelecionado.classList.remove('abaSelecionado');
-
-  const idContainerAdiconarNovo = `container-${botaoAdicionar.id}`;
-  console.log(idContainerAdiconarNovo);
-  const conteudoASerMostrado = document.getElementById(idContainerAdiconarNovo);
-  conteudoASerMostrado.classList.add('abaSelecionado');
+    const idContainerSessaoaba = `container-${aba.id}`
+    const conteudoASerMostrado = document.getElementById(idContainerSessaoaba)
+    conteudoASerMostrado.classList.add('abaSelecionado')
   })
+})
+
+const botaoAdicionar = document.querySelectorAll('.adicionar')
+botaoAdicionar.forEach(botao => {
+  botao.addEventListener('click', () => {
+    const sessaoSelecionado = document.querySelector(
+      '.abaConteudo.abaSelecionado'
+    )
+    sessaoSelecionado.classList.remove('abaSelecionado')
+
+    const idContainerAdicionar = `container-${botao.id}`
+    console.log(idContainerAdicionar)
+    const conteudoASerMostrado = document.getElementById(idContainerAdicionar)
+    conteudoASerMostrado.classList.add('abaSelecionado')
+  })
+})
