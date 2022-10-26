a6a0644eb1ebb9a8b233a266b8a04aa11ed8035d
Iago Augusto <iagoalvesaugusto@gmail.com>
2022-10-26 07:20:08 -0300

    Terminado telas de paciente, começando a criar a responsividade

--- view/script.js
+++ view/script.js
@@ -1,18 +1,38 @@
-function mostrarOcultarSenha() {
-  var senha = document.getElementById('senha')
-  if (senha.type == 'password') {
-    senha.type = 'text'
-  } else {
-    senha.type = 'password'
-  }
-}
+// const menu = document.querySelector(".menu");
+// // const menuItems = document.querySelectorAll(".menuItem");
+// const hamburger= document.querySelector(".hamburger");
+// const closeIcon= document.querySelector(".closeIcon");
+// const menuIcon = document.querySelector(".menuIcon");
 
+// function toggleMenu() {
+//   if (menu.classList.contains("mostrarMenu")) {
+//     menu.classList.remove("mostrarMenu");
+//     closeIcon.style.display = "none";
+//     menuIcon.style.display = "block";
+//   } else {
+//     menu.classList.add("showMenu");
+//     closeIcon.style.display = "block";
+//     menuIcon.style.display = "none";
+//   }
+// }
 
-function mostrarOcultarConfirmarSenha() {
-  var senha = document.getElementById('confirmarSenha')
-  if (senha.type == 'password') {
-    senha.type = 'text'
-  } else {
-    senha.type = 'password'
+// hamburger.addEventListener("click", toggleMenu);
+
+const hamburger = document.querySelector(".hamburger")
+const menuIcon = document.querySelector(".menuIcon")
+const closeIcon = document.querySelector(".closeIcon")
+
+hamburger.addEventListener("click", ()=> {
+  const menu = document.querySelector(".menu")
+
+  if (menu.classList.contains("mostrarMenu")){
+    menu.classList.remove("mostrarMenu")
+    closeIcon.classList.remove("selecionadoIcon")
+    menuIcon.classList.add("selecionadoIcon")
+  }else{
+    menu.classList.add("mostrarMenu")
+    menuIcon.classList.remove("selecionadoIcon")
+    closeIcon.classList.add("selecionadoIcon")
+
   }
-}
+});
