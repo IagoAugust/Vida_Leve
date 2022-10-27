137af063b392c6a96927adb5abf67eefe27a613b
Iago Augusto <iagoalvesaugusto@gmail.com>
2022-10-26 21:02:53 -0300

    Arrumando responsividade

--- view/script.js
+++ view/script.js
@@ -1,24 +1,6 @@
-// const menu = document.querySelector(".menu");
-// // const menuItems = document.querySelectorAll(".menuItem");
-// const hamburger= document.querySelector(".hamburger");
-// const closeIcon= document.querySelector(".closeIcon");
-// const menuIcon = document.querySelector(".menuIcon");
 
-// function toggleMenu() {
-//   if (menu.classList.contains("mostrarMenu")) {
-//     menu.classList.remove("mostrarMenu");
-//     closeIcon.style.display = "none";
-//     menuIcon.style.display = "block";
-//   } else {
-//     menu.classList.add("showMenu");
-//     closeIcon.style.display = "block";
-//     menuIcon.style.display = "none";
-//   }
-// }
 
-// hamburger.addEventListener("click", toggleMenu);
-
-const hamburger = document.querySelector(".hamburger")
+const hamburger = document.querySelector("#btnHamburger")
 const menuIcon = document.querySelector(".menuIcon")
 const closeIcon = document.querySelector(".closeIcon")
 
@@ -26,10 +8,14 @@ hamburger.addEventListener("click", ()=> {
   const menu = document.querySelector(".menu")
 
   if (menu.classList.contains("mostrarMenu")){
+    hamburger.classList.add("hamburger")
+    hamburger.classList.remove("hamburger-close")
     menu.classList.remove("mostrarMenu")
     closeIcon.classList.remove("selecionadoIcon")
     menuIcon.classList.add("selecionadoIcon")
   }else{
+    hamburger.classList.remove("hamburger")
+    hamburger.classList.add("hamburger-close")
     menu.classList.add("mostrarMenu")
     menuIcon.classList.remove("selecionadoIcon")
     closeIcon.classList.add("selecionadoIcon")
