76f0ef137889508e7808ec357edd32de47058743
Iago Augusto <iagoalvesaugusto@gmail.com>
2022-10-26 22:27:23 -0300

    Alterando responsividade do mobile

--- view/paciente.js
+++ view/paciente.js
@@ -36,3 +36,33 @@ botaoAdicionar.forEach(botao => {
     conteudoASerMostrado.classList.add('abaSelecionado')
   })
 })
+
+
+
+
+const hamburger = document.querySelector("#btnHamburger")
+
+
+hamburger.addEventListener("click", ()=> {
+  const menu = document.querySelector(".menu")
+  const menuIcon = document.querySelector(".menuIcon")
+  const closeIcon = document.querySelector(".closeIcon")
+  const voltarIcone = document.querySelector("#icone-volar-mobile")
+
+  if (menu.classList.contains("mostrarMenu")){
+    hamburger.classList.add("hamburger")
+    hamburger.classList.remove("hamburger-close")
+    voltarIcone.classList.remove("voltar-mobile")
+    menu.classList.remove("mostrarMenu")
+    closeIcon.classList.remove("selecionadoIcon")
+    menuIcon.classList.add("selecionadoIcon")
+  }else{
+    hamburger.classList.remove("hamburger")
+    hamburger.classList.add("hamburger-close")
+    voltarIcone.classList.add("voltar-mobile")
+    menu.classList.add("mostrarMenu")
+    menuIcon.classList.remove("selecionadoIcon")
+    closeIcon.classList.add("selecionadoIcon")
+
+  }
+});
--- view/script.js
+++ view/script.js
@@ -8,14 +8,10 @@ hamburger.addEventListener("click", ()=> {
   const menu = document.querySelector(".menu")
 
   if (menu.classList.contains("mostrarMenu")){
-    hamburger.classList.add("hamburger")
-    hamburger.classList.remove("hamburger-close")
     menu.classList.remove("mostrarMenu")
     closeIcon.classList.remove("selecionadoIcon")
     menuIcon.classList.add("selecionadoIcon")
   }else{
-    hamburger.classList.remove("hamburger")
-    hamburger.classList.add("hamburger-close")
     menu.classList.add("mostrarMenu")
     menuIcon.classList.remove("selecionadoIcon")
     closeIcon.classList.add("selecionadoIcon")
